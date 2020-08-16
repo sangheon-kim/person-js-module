028292e1f846c9be4ae7d43e775b9e75070e002d
sangheon-kim <ksj83671@gmail.com>
2020-08-16 18:45:10 +0900

    [김상헌] 직접짰음

---
+++ README.md
@@ -0,0 +1,5 @@
+# Person Recycle Module
+
+## Made By Sangheon.kim
+
+### 1. 캘린더 모듈 (calendar.js)
---
+++ index.js
@@ -0,0 +1,6 @@
+const { calendar } = require("./calendar");
+
+calendar.setToday(2020, 09);
+calendar.getPrevCalendar();
+// calendar.getToday();
+// calendar.getPrevCalendar();
